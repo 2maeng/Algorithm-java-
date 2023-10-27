54ba5d42e8af05aff0f283bcea85036efaa584f6
2maeng <118092642+2maeng@users.noreply.github.com>
2023-10-27 14:06:18 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string) {
+        
+        StringBuilder stringBuilder = new StringBuilder(my_string);
+        
+        return stringBuilder.reverse().toString();
+    }
+}
