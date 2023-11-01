2e9550c5ccbc73429227851b99731a54235acc37
2maeng <118092642+2maeng@users.noreply.github.com>
2023-11-01 10:26:01 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 0.06 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < my_string.length(); i++) {
+            char character = my_string.charAt(i);
+            for (int j = 0; j < n; j++) {
+                answer.append(character);
+            }           
+        }
+        
+        return answer.toString();
+    }
+}
